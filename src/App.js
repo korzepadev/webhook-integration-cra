f02d73e583d372ab9c6727ae0c89322ab4546f60
korzepadawid <56738926+korzepadawid@users.noreply.github.com>
2021-05-18 18:40:09 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          test 18:37
+          test 18:40
         </p>
         <a
           className="App-link"
