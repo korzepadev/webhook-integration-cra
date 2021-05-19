c5708d0bf21ecc76d04241af6ac9c2d8e89f429c
korzepadawid <56738926+korzepadawid@users.noreply.github.com>
2021-05-19 15:02:42 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,18 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          test 18:40
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn tin
-        </a>
+        it's a test
       </header>
     </div>
   );
