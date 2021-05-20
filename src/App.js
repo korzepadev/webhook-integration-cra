85ce837dd9095b66c55da63d6d3918a591e8d56e
korzepadawid <korzepadawid@yahoo.com>
2021-05-20 11:37:35 +0200

    added reeset button

--- src/App.js
+++ src/App.js
@@ -5,12 +5,14 @@ function App() {
 
   const incrementCount = () => setCount(count + 1);
   const decrementCount = () => setCount(count - 1);
+  const resetCount = () => setCount(0);
 
   return (
     <div>
       <h1>count: {count}</h1>
       <button onClick={incrementCount}>+</button>
       <button onClick={decrementCount}>-</button>
+      <button onClick={resetCount}>reset</button>
     </div>
   );
 }
